65127587c69ce2dc5e919482ad06de8fba84e11c
AlexandroSnd <sundukovalexandr06@yandex.ru>
2025-04-28 17:17:05 +0500

    fix

--- src/cli.ts
+++ src/cli.ts
@@ -6,15 +6,18 @@ const program = new Command();
 program
   .name("qr-cli")
   .description("Генератор QR-кодов для терминала")
-  .version("1.0.0");
-
-program
+  .version("1.0.0")
   .command("generate")
   .description("Генерирует QR-код из текста или ссылки")
   .argument("<text>", "Текст или ссылка для генерации QR-кода")
-  .option("--size <number>", "Размер QR-кода", (value) => parseInt(value, 10), 8)
+  .option(
+    "--size <number>",
+    "Размер QR-кода",
+    (value) => parseInt(value, 10),
+    8
+  )
   .action((text, sizeObj) => {
-    const size = parseInt(sizeObj.size)
+    const size = parseInt(sizeObj.size);
     if (isNaN(size) || size < 1) {
       console.error("Ошибка: Неверный размер QR-кода.");
       process.exit(1);
